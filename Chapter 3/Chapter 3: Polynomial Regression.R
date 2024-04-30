abb49b5e264075acab3ce7970baaf0f59288c3cb
Lekan Ali <77731827+Lekan-Ali@users.noreply.github.com>
2024-04-30 13:04:44 +0100

    Update Chapter 3: Polynomial Regression.R

--- Chapter 3/Chapter 3: Polynomial Regression.R
+++ Chapter 3/Chapter 3: Polynomial Regression.R
@@ -21,7 +21,27 @@ summary(lm.cubic)
 
 
 
-
 #Step 4: comparing the simple linear model to quadratic polynomial model to verify the better model
 #see Table 3.8 in Dissertation
 anova(lm.simple, lm.quadratic)
+
+
+
+#Step 5: Cubic and polynomial fit applied to the Rating and Balance scatterplot
+# see Figure 3.5 in dissertation
+#Plotting polynomial line
+plot(Credit$Rating, Credit$Balance
+     , pch = 1, cex = 0.5
+     , xlab = "Rating", ylab = "Balance"
+     , lty = 2, family = "mono"
+     , col = alpha(1,0.6))
+
+# quadratic lines in red
+pred <- predict(lm.quadratic)
+ix <- sort(Credit$Rating, index.return=T)$ix
+lines(Credit$Rating[ix], pred[ix], col = "red", lwd = 2
+      , lty =1)
+
+#linear regression line in blue
+abline(lm.simple, col = "blue", lwd = 2
+       , lty = 1)
